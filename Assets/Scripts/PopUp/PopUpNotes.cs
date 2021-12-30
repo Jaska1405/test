86693b8a6bc37ecd95db1ecc307f869198236e8e
Jaska1405 <jaskamanurung2001@gmail.com>
2021-12-30 18:06:24 +0700

    test

---
+++ Assets/Scripts/PopUp/PopUpNotes.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpNotes : MonoBehaviour
+{
+    [SerializeField] public GameObject imageNote;
+    [SerializeField] public GameObject panelNote;
+    [SerializeField] public GameObject interactUI;
+    [SerializeField] public GameObject tombolNex;
+    [SerializeField] public GameObject tombolPrev; 
+    
+    public Animator anim;
+
+    public string note;
+    public bool PlayerInRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && PlayerInRange)
+        {
+            if (imageNote.activeInHierarchy)
+            {
+                
+                imageNote.SetActive(false);
+                panelNote.SetActive(false);
+                tombolNex.SetActive(true);
+                tombolPrev.SetActive(true);
+                
+            }
+            else
+            {
+                
+                imageNote.SetActive(true);
+                panelNote.SetActive(true);
+                tombolNex.SetActive(true);
+                tombolPrev.SetActive(true);
+                // dialogText.text = dialog;
+                
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && PlayerInRange)
+        {
+            anim.SetBool("IsOpen", true);
+        }
+        else
+         {
+            anim.SetBool("IsOpen", false);
+         }
+        
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.SetActive(true);
+            PlayerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.SetActive(false);
+            PlayerInRange = false;
+            imageNote.SetActive(false);
+            panelNote.SetActive(false);
+            tombolNex.SetActive(false);
+            tombolPrev.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PausePanel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausePanel : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject panelPause;
+    //public GameObject panelSetting;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+                
+            }
+            else
+            {
+                PauseGame();
+                
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        panelPause.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false ;
+    }
+   
+
+    void PauseGame()
+    {
+        panelPause.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    
+
+
+
+    public void menu ()
+  {
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("Testing");
+        Debug.Log("Load menu.....");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quiting Game...");
+    }
+
+}
